47ab5675ee8cbdf07b97f37310ed6bcd69fab00d
fravellog <fr.avellog@duocuc.cl>
2025-08-27 14:49:41 -0400

    borrado rapido en caso de inicio de sesion fallido

--- funciones.js
+++ funciones.js
@@ -6,16 +6,30 @@ function inicioSesion() {
         document.getElementById("errorDos").textContent = "";
         document.getElementById("errorUser").textContent = "";
         document.getElementById("errorContra").textContent = "";
-        if (usuario === "" && contrasena === "") {
+        if (usuario == "" && contrasena == "") {
             document.getElementById("errorDos").textContent = "Por favor, ingrese su nombre de usuario y contraseña";
             return false; // Evitar el envío del formulario
         
-        } else if (usuario === "") {
+        } else if (usuario == "") {
             document.getElementById("errorUser").textContent = "Por favor, ingrese su nombre de usuario";
             return false; // Evitar el envío del formulario
 
-        } else if (contrasena === "") {
+        } else if (contrasena == "") {
             document.getElementById("errorContra").textContent = "Por favor, ingrese su contraseña";
+            return false; // Evitar el envío del formulario
+
+        } else if (contrasena != "1234") {
+            document.getElementById("errorContra").textContent = "Contraseña incorrecta";
+            document.getElementById("password").select();
+            return false; // Evitar el envío del formulario
+
+        } else if (usuario != "admin") {
+            document.getElementById("errorUser").textContent = "Usuario incorrecto";
+            document.getElementById("username").select();
+            return false; // Evitar el envío del formulario
+            
+        } else if (usuario != "admin" && contrasena != "1234") {
+            document.getElementById("errorDos").textContent = "Usuario y contraseña incorrectos";
             return false; // Evitar el envío del formulario
 
         } else {
